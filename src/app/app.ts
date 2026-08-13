389781f4c41f4fc06c4772504a6d263369c49693
Abhishek Mishra <gauravmishra2044@gmail.com>
2026-08-13 22:34:42 +0530

    added new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventComp } from './components/event-comp/event-comp';
+import { SignalComp } from './components/signal-comp/signal-comp';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EventComp,SignalComp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
