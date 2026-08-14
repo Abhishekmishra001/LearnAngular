321a4a036eef091bb26512687053063569197f0a
Abhishek Mishra <gauravmishra2044@gmail.com>
2026-08-14 19:18:05 +0530

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { EventComp } from './components/event-comp/event-comp';
+import { SignalComp } from './components/signal-comp/signal-comp';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path:'',
+     redirectTo:'SignalComp'
+    },
+    {
+        path:'eventComp',
+        component:EventComp
+    },
+    {
+        path:'signalComp',
+        component:SignalComp
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { EventComp } from './components/event-comp/event-comp';
 import { SignalComp } from './components/signal-comp/signal-comp';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,EventComp,SignalComp],
+  imports: [RouterOutlet, EventComp, SignalComp, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
